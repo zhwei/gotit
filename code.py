6fd1355c90496bf9a22d7e20f696714ba8dc72df
zhwei <sdutzhangwei@gamil.com>
2013-01-20 20:41:12 +0800

    sort 1

--- code.py
+++ code.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
-
 import web
 from web import form
 web.config.debug = False
@@ -15,6 +14,7 @@ from BeautifulSoup import BeautifulSoup
 #self
 from calc_GPA import GPA
 from get_CET import CET
+from zheng import ZHENG
 
 
 urls = (
@@ -29,26 +29,26 @@ urls = (
 render = web.template.render('template/') # your templates
 
 #urls
-def get_first():
-    url1 = "http://210.44.176.133"
-    u1 = urllib2.urlopen(url1)
-    url2 = u1.geturl()
-    url3 = url2[:-13]
-    return url3
+# def get_first():
+#     url1 = "http://210.44.176.133"
+#     u1 = urllib2.urlopen(url1)
+#     url2 = u1.geturl()
+#     url3 = url2[:-13]
+#     return url3
 
 
-def get_login(url3):
-    bbb = url3 + "Default3.aspx"
-    return bbb
+# def get_login(url3):
+#     bbb = url3 + "Default3.aspx"
+#     return bbb
 
-#get url
-def get_url(_xh,funl,url3):
-    url= url3 + funl +'.aspx?xh=' + _xh 
-    print url
-    return url
+# #get url
+# def get_url(_xh,funl,url3):
+#     url= url3 + funl +'.aspx?xh=' + _xh 
+#     print url
+#     return url
 
 
-#timetable_url = 'http://210.44.176.132/xskbcx.aspx?xh=%s'% _xh 
+# #timetable_url = 'http://210.44.176.132/xskbcx.aspx?xh=%s'% _xh 
 
 
 def login(login_url,_xh,_pw):
@@ -77,39 +77,39 @@ def login(login_url,_xh,_pw):
         return "error"
 
 
-def get_score(score_url, opener,tname):
-    #get result page
-    if opener == "error":
-        out = ["<div class='alert alert-error'><h4><center><strong>Login ERROR!!!</strong><center></h4></div>",]
-        return out
-    else:
-        score_html = opener.open(score_url).read()
-        if score_html:
-            #print score_html
-            print('html is ok')
-        #get table
-        soup = BeautifulSoup(score_html, fromEncoding='gbk')
-        table = soup.find("table", {"id": tname}) #table is class
-        if table:
-            out = table.contents
-        else:
-            out = ["<div class='alert alert-error'><h4><center><strong>System is Busy!!!</strong><center></h4></div>",]
-        return out
+# def get_score(score_url, opener,tname):
+#     #get result page
+#     if opener == "error":
+#         out = ["<div class='alert alert-error'><h4><center><strong>Login ERROR!!!</strong><center></h4></div>",]
+#         return out
+#     else:
+#         score_html = opener.open(score_url).read()
+#         if score_html:
+#             #print score_html
+#             print('html is ok')
+#         #get table
+#         soup = BeautifulSoup(score_html, fromEncoding='gbk')
+#         table = soup.find("table", {"id": tname}) #table is class
+#         if table:
+#             out = table.contents
+#         else:
+#             out = ["<div class='alert alert-error'><h4><center><strong>System is Busy!!!</strong><center></h4></div>",]
+#         return out
 
-#forms
-info_form = form.Form(
-        form.Textbox("number", description="学号:",class_="span3",pre="&nbsp;&nbsp;"),
-        form.Password("password", description="密码:",class_="span3",pre="&nbsp;&nbsp;"),
-        form.Dropdown('Type',[('1', '成绩查询'), ('2', '考试时间查询'), ('3', '课表查询'),('4', '平均学分绩点查询')],description="查询类型:",pre="&nbsp;&nbsp;"),
-        validators = [
-            form.Validator('输入不合理!', lambda i:int(i.number) > 9)]
-        )
-cet_form = form.Form(
-        form.Textbox("zkzh", description="准考证号:",class_="span3",pre="&nbsp;&nbsp;"),
-        form.Textbox("name", description="姓名:",class_="span3",pre="&nbsp;&nbsp;"),
-        validators = [
-            form.Validator('输入不合理!', lambda i:int(i.zkzh) != 15)]
-        )
+# #forms
+# info_form = form.Form(
+#         form.Textbox("number", description="学号:",class_="span3",pre="&nbsp;&nbsp;"),
+#         form.Password("password", description="密码:",class_="span3",pre="&nbsp;&nbsp;"),
+#         form.Dropdown('Type',[('1', '成绩查询'), ('2', '考试时间查询'), ('3', '课表查询'),('4', '平均学分绩点查询')],description="查询类型:",pre="&nbsp;&nbsp;"),
+#         validators = [
+#             form.Validator('输入不合理!', lambda i:int(i.number) > 9)]
+#         )
+# cet_form = form.Form(
+#         form.Textbox("zkzh", description="准考证号:",class_="span3",pre="&nbsp;&nbsp;"),
+#         form.Textbox("name", description="姓名:",class_="span3",pre="&nbsp;&nbsp;"),
+#         validators = [
+#             form.Validator('输入不合理!', lambda i:int(i.zkzh) != 15)]
+#         )
 
 #成绩查询
 class index:
@@ -125,45 +125,42 @@ class index:
             _xh = form.d.number
             _pw = form.d.password
             t = form.d.Type
-            url3 = get_first()
-            login_url = get_login(url3)    
-            if login(login_url,_xh,_pw):
-                opener = login(login_url, _xh,_pw)
-                if t == "1":
-                    tname = "DataGrid1"
-                    funl = "xscjcx_dq"
-                elif t == "2":
-                    tname = "DataGrid1"
-                    funl = "xskscx"
-                elif t == "3":
-                    tname = "Table1"
-                    funl = "xskbcx"
-                elif t == "4":
-                    gpa = GPA(_xh)
-                    a = gpa.get_gpa()
-                    table = [
-                        # "<tr><td><strong>姓名</strong></td><td><strong>" + str(a["name"])+"</strong></td></tr>",
-                        # "<tr><td><strong>班级</strong></td><td><strong>" + str(a["class"])+"</strong></td></tr>",
-                        "<tr><td><strong>平均学分绩点</strong></td><td><strong>" + str(a["ave_score"])+"</strong></td></tr>",
-                        # "<tr><td><strong>总基点</strong></td><td><strong>" + str(a["total_score"])+"</strong></td></tr>",
-                        # "<tr><td><strong>总学分</strong></td><td><strong>" + str(a["totle_credits"])+"</strong></td></tr>",
-                        # "<tr><td><strong>至今未通过科目</strong></td><td><strong>" + str(len(a['not_accept']))+"</strong></td></tr>",
-                    ]
-                    error = None
-                    return render.result(table,error)
-                else:
-                    return '<script type="text/javascript">alert("输入不合理!");top.location="/"</script>'
+            if t == "1":
+                funl = "xscjcx_dq"
+                cet = ZHENG()
+                table = 
+            elif t == "2":
+                tname = "DataGrid1"
+                funl = "xskscx"
+            elif t == "3":
+                tname = "Table1"
+                funl = "xskbcx"
+            elif t == "4":
+                gpa = GPA(_xh)
+                a = gpa.get_gpa()
+                table = [
+                    # "<tr><td><strong>姓名</strong></td><td><strong>" + str(a["name"])+"</strong></td></tr>",
+                    # "<tr><td><strong>班级</strong></td><td><strong>" + str(a["class"])+"</strong></td></tr>",
+                    "<tr><td><strong>平均学分绩点</strong></td><td><strong>" + str(a["ave_score"])+"</strong></td></tr>",
+                    # "<tr><td><strong>总基点</strong></td><td><strong>" + str(a["total_score"])+"</strong></td></tr>",
+                    # "<tr><td><strong>总学分</strong></td><td><strong>" + str(a["totle_credits"])+"</strong></td></tr>",
+                    # "<tr><td><strong>至今未通过科目</strong></td><td><strong>" + str(len(a['not_accept']))+"</strong></td></tr>",
+                ]
+                error = None
+                return render.result(table,error)
+            else:
+                return '<script type="text/javascript">alert("输入不合理!");top.location="/"</script>'
 
-                score_url = get_url(_xh,funl,url3)
-                table = get_score(score_url, opener,tname)
+            score_url = get_url(_xh,funl,url3)
+            table = get_score(score_url, opener,tname)
 
-                if table:
-                    error = None
-                    return render.result(table, error)
-                else:
-                    table = None
-                    error = "can not find your index table"
-                    return render.result(table, error)
+            if table:
+                error = None
+                return render.result(table, error)
+            else:
+                table = None
+                error = "can not find your index table"
+                return render.result(table, error)
 
 #cet
 class cet:
