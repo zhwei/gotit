b5cdee84c076238ef11e162571ea83e970c3246f
zhwei <sdutzhangwei@gamil.com>
2013-01-20 21:10:44 +0800

    move zheng out of code.py

--- code.py
+++ code.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import web
 from web import form
-web.config.debug = False
+web.config.debug = True
 
 import random
 import re
@@ -28,88 +28,20 @@ urls = (
 
 render = web.template.render('template/') # your templates
 
-#urls
-# def get_first():
-#     url1 = "http://210.44.176.133"
-#     u1 = urllib2.urlopen(url1)
-#     url2 = u1.geturl()
-#     url3 = url2[:-13]
-#     return url3
-
-
-# def get_login(url3):
-#     bbb = url3 + "Default3.aspx"
-#     return bbb
-
-# #get url
-# def get_url(_xh,funl,url3):
-#     url= url3 + funl +'.aspx?xh=' + _xh 
-#     print url
-#     return url
-
-
-# #timetable_url = 'http://210.44.176.132/xskbcx.aspx?xh=%s'% _xh 
-
-
-def login(login_url,_xh,_pw):
-    if login_url: 
-        html1 = urllib.urlopen(login_url).read()
-    else:
-        print("login url is error")
-
-    a = re.compile('<input type="hidden" name="__VIEWSTATE" value="(.*)" />')
-    try:
-        VIEWSTATE = a.findall(html1)[0]
-    except:
-        print("can not get viewstate")
-    #cookie
-    student_cookie = cookielib.CookieJar()
-    #login
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(student_cookie))
-    data = '__VIEWSTATE='+VIEWSTATE+'&TextBox1='+_xh+'&TextBox2='+_pw+'&ddl_js=%D1%A7%C9%FA&Button1=+%B5%C7+%C2%BC+'
-    try:
-        login_request = urllib2.Request(login_url, data)
-        opener.open(login_request, data)
-        # print opener
-        return opener
-    except:
-        # out = ["<div class='alert alert-error'><h4><center><strong>Login ERROR!!!</strong><center></h4></div>",]
-        return "error"
-
-
-# def get_score(score_url, opener,tname):
-#     #get result page
-#     if opener == "error":
-#         out = ["<div class='alert alert-error'><h4><center><strong>Login ERROR!!!</strong><center></h4></div>",]
-#         return out
-#     else:
-#         score_html = opener.open(score_url).read()
-#         if score_html:
-#             #print score_html
-#             print('html is ok')
-#         #get table
-#         soup = BeautifulSoup(score_html, fromEncoding='gbk')
-#         table = soup.find("table", {"id": tname}) #table is class
-#         if table:
-#             out = table.contents
-#         else:
-#             out = ["<div class='alert alert-error'><h4><center><strong>System is Busy!!!</strong><center></h4></div>",]
-#         return out
-
-# #forms
-# info_form = form.Form(
-#         form.Textbox("number", description="学号:",class_="span3",pre="&nbsp;&nbsp;"),
-#         form.Password("password", description="密码:",class_="span3",pre="&nbsp;&nbsp;"),
-#         form.Dropdown('Type',[('1', '成绩查询'), ('2', '考试时间查询'), ('3', '课表查询'),('4', '平均学分绩点查询')],description="查询类型:",pre="&nbsp;&nbsp;"),
-#         validators = [
-#             form.Validator('输入不合理!', lambda i:int(i.number) > 9)]
-#         )
-# cet_form = form.Form(
-#         form.Textbox("zkzh", description="准考证号:",class_="span3",pre="&nbsp;&nbsp;"),
-#         form.Textbox("name", description="姓名:",class_="span3",pre="&nbsp;&nbsp;"),
-#         validators = [
-#             form.Validator('输入不合理!', lambda i:int(i.zkzh) != 15)]
-#         )
+#forms
+info_form = form.Form(
+        form.Textbox("number", description="学号:",class_="span3",pre="&nbsp;&nbsp;"),
+        form.Password("password", description="密码:",class_="span3",pre="&nbsp;&nbsp;"),
+        form.Dropdown('Type',[('1', '成绩查询'), ('2', '考试时间查询'), ('3', '课表查询'),('4', '平均学分绩点查询')],description="查询类型:",pre="&nbsp;&nbsp;"),
+        validators = [
+            form.Validator('输入不合理!', lambda i:int(i.number) > 9)]
+        )
+cet_form = form.Form(
+        form.Textbox("zkzh", description="准考证号:",class_="span3",pre="&nbsp;&nbsp;"),
+        form.Textbox("name", description="姓名:",class_="span3",pre="&nbsp;&nbsp;"),
+        validators = [
+            form.Validator('输入不合理!', lambda i:int(i.zkzh) != 15)]
+        )
 
 #成绩查询
 class index:
@@ -125,16 +57,13 @@ class index:
             _xh = form.d.number
             _pw = form.d.password
             t = form.d.Type
+            
             if t == "1":
-                funl = "xscjcx_dq"
-                cet = ZHENG()
-                table = 
+                func = "xscjcx_dq"
             elif t == "2":
-                tname = "DataGrid1"
-                funl = "xskscx"
+                func = "xskscx"
             elif t == "3":
-                tname = "Table1"
-                funl = "xskbcx"
+                func = "xskbcx"
             elif t == "4":
                 gpa = GPA(_xh)
                 a = gpa.get_gpa()
@@ -151,8 +80,8 @@ class index:
             else:
                 return '<script type="text/javascript">alert("输入不合理!");top.location="/"</script>'
 
-            score_url = get_url(_xh,funl,url3)
-            table = get_score(score_url, opener,tname)
+            zheng = ZHENG(_xh,_pw,func)
+            table = zheng.get_table()
 
             if table:
                 error = None
--- zheng.py
+++ zheng.py
@@ -23,8 +23,9 @@ class ZHENG:
     def __get_base_url(self):
         target = urllib2.urlopen(URL)
         with_random_url = target.geturl()
-        base_url = with_random_url[-13]
+        base_url = with_random_url[:-13]
         return base_url
+
     def __get_login_url(self):
         base_url = self.__get_base_url()
         login_url = base_url + "Default3.aspx"
@@ -32,76 +33,48 @@ class ZHENG:
 
     def __get_url(self):
         base_url = self.__get_base_url()
-        func_url = base_url + self.func + "aspx?xh=" + self._xh
+        func_url = base_url + self.func + ".aspx?xh=" + self._xh
         return func_url
     
     def login(self):
-        login_url = __get_login_url()
-
+        login_url = self.__get_login_url()
         html1 = urllib.urlopen(login_url).read()
         a = re.compile('<input type="hidden" name="__VIEWSTATE" value="(.*)" />')
-        try:
-            VIEWSTATE = a.findall(html1)[0]
-        except:
-            print("can not get viewstate")
+        VIEWSTATE = a.findall(html1)[0]
         #cookie
         student_cookie = cookielib.CookieJar()
         #login
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(student_cookie))
         data = '__VIEWSTATE='+VIEWSTATE+'&TextBox1='+self._xh+'&TextBox2='+self._pw+'&ddl_js=%D1%A7%C9%FA&Button1=+%B5%C7+%C2%BC+'
-
         login_request = urllib2.Request(login_url, data)
-        opener.open(login_request, data)
         status = opener.open(login_request, data).read()
-        print(status.decode("gb2312").encode("utf-8"))
-        string = "密码错误！！"
-        key = string.decode('utf-8').encode('gb2312')
-        t = status.find(key)
-
-        return opener
-        except:
-            # out = ["<div class='alert alert-error'><h4><center><strong>Login ERROR!!!</strong><center></h4></div>",]
-            return "error"
-
-
-
-    def get_table(self):
-        login_url = self.__get_base_url()
-        login_html = urllib2.urlopen(login_url).read()
-        view_re = re.compile('<input type="hidden" name="__VIEWSTATE" value="(.*)" />')
-        VIEWSTATE = view_re.findall(login_html)[0]
-
-        student_cookie = cookielib.CookieJar()
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(student_cookie))
-        data = '__VIEWSTATE='+VIEWSTATE+'&TextBox1='+self._xh+'&TextBox2='+self._pw+'&ddl_js=%D1%A7%C9%FA&Button1=+%B5%C7+%C2%BC+'
-        login_request = urllib2.Request(login_url, data)
-        opener.open(login_request, data)
-
-        #验证是否登录成功
-        status = opener.open(login_request, data).read()
-        print(status.decode("gb2312").encode("utf-8"))
         string = "密码错误！！"
         key = string.decode('utf-8').encode('gb2312')
         t = status.find(key)
         if t != -1:
-            result = [u"<div class='alert alert-error'><h4><center><strong>用户名或密码错误!</strong><center></h4></div>",]
-            return result
+            return None
         else:
+            return opener
+
+
+    def get_table(self):
+        opener = self.login()
+        if opener:
             target_url = self.__get_url()
-            print "++++++++++++++++++++++++++"
+            print target_url
             target_html = opener.open(target_url).read()
-            # print target_html
-            #get table
             soup = BeautifulSoup(target_html, fromEncoding='gbk')
             if self.func == "xskbcx":
                 table_name = "Table1"
             else:
                 table_name = "DataGrid1"
+
             table = soup.find("table", {"id": table_name}) #table is class
-            # if table:
             result = table.contents
-            # else:
-                # result = ["<div class='alert alert-error'><h4><center><strong>System is Busy,Please Try Again!</strong><center></h4></div>",]
+            return result
+
+        else:
+            result = [u"<div class='alert alert-error'><h4><center><strong>用户名或密码错误!</strong><center></h4></div>",]
             return result
 
 
