c56bc14d76c1b840261ba5a2165a32654d0af326
zhwei <sdutzhangwei@gamil.com>
2013-07-11 23:11:51 +0800

    133 on

--- addons/config.py
+++ addons/config.py
@@ -3,18 +3,16 @@
 
 #正方教务系统
 ##教务系统url,注意不要忘记最后的"/"
-zf_url = "http://210.44.176.132/"
+zf_url = "http://210.44.176.133/"
 ##url中是否有随机字符串
-random = False
+random = True
 
 #成绩查询网址
 score_url = "http://210.44.176.116/cjcx/zcjcx_list.php"
 
 #四六级成绩查询
 
-##POST地址
-cet_url = "http://www.chsi.com.cn/cet/query"
-##是否使用bae查询
+##POST地址 cet_url = "http://www.chsi.com.cn/cet/query" ##是否使用bae查询
 baefetch = True
 
 #查询页缓存时间(秒)
