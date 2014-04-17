76ee9d850631df3dc445f657db58215f8b51061c
zhwei <sdutzhangwei@gmail.com>
2014-04-17 19:56:35 +0800

    share session with subapp

--- gotit.py
+++ gotit.py
@@ -23,8 +23,23 @@ mapping = (
     "manage.gotit.asia", manage.app,
 )
 
+
 # main app
 app = web.subdomain_application(mapping, globals(), autoreload=False)
+
+# session
+if web.config.get('_session') is None:
+    session = web.session.Session(app, RedisStore(), {'count': 0, 'xh':False})
+    web.config._session = session
+else:
+    session = web.config._session
+
+# web server
+def session_hook():
+    """ share session with sub apps
+    """
+    web.ctx.session = session
+app.add_processor(web.loadhook(session_hook))
 
 # for gunicorn
 application = app.wsgifunc()
